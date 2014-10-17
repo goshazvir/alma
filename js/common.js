89dad9b1a29f97eba041b73179a597a86f254109
Georgiy <goshazvir@gmail.com>
2014-10-17 03:28:49 +0300

    upd

--- js/common.js
+++ js/common.js
@@ -7,4 +7,47 @@ head.ready(function() {
 	    });
 	    return false;
 		});
+
+		$('.js-flexslider').flexslider({
+			namespace: ".gallery-",
+			smoothHeight: true,
+			animation: "slide",
+			selector: ".js-flexslider-in > .gallery-item",
+			directionNav: false,
+			// controlsContainer: ".slider",
+			useCSS: false,
+			controlNav: false,
+			slideshow: false
+		});
+		$('.gallery-prev').on('click', function(){
+		    $('.js-flexslider').flexslider('prev')
+		    return false;
+		})
+
+		$('.gallery-next').on('click', function(){
+		    $('.js-flexslider').flexslider('next')
+		    return false;
+		})
+
+
+		$('.js-minislide').flexslider({
+			namespace: ".minislide-",
+			smoothHeight: true,
+			animation: "slide",
+			selector: ".js-minislide-in > .minislide-item",
+			directionNav: false,
+			// controlsContainer: ".slider",
+			useCSS: false,
+			controlNav: false,
+			slideshow: false
+		});
+		$('.minislide-prev').on('click', function(){
+		    $('.js-minislide').flexslider('prev')
+		    return false;
+		})
+
+		$('.minislide-next').on('click', function(){
+		    $('.js-minislide').flexslider('next')
+		    return false;
+		})
 });
